837babfc8ddfe3d2a7986cd24d4e8b4c6cbd353a
Jamal Sfenjeh <78261740+jamal-and@users.noreply.github.com>
2025-07-05 11:49:52 +0400

    Create style-2.zsh

---
+++ style-2.zsh
@@ -0,0 +1,12 @@
+parse_git_branch() {
+    git branch 2> /dev/null | sed -n -e 's/^\* \(.*\)/[\1]/p'
+}
+COLOR_DEF='%f'
+COLOR_USR='%F{243}'
+COLOR_DIR='%F{197}'
+COLOR_GIT='%F{39}'
+# About the prefixed $: https://tldp.org/LDP/Bash-Beginners-Guide/html/sect_03_03.html#:~:text=Words%20in%20the%20form%20%22%24',by%20the%20ANSI%2DC%20standard.
+NEWLINE=$'\n'
+# Set zsh option for prompt substitution
+setopt PROMPT_SUBST
+export PROMPT='${COLOR_USR}%n@%m%f ${COLOR_DIR}%~%f ${COLOR_GIT}$(parse_git_branch)${COLOR_DEF}${NEWLINE}%% '
